236157fa02e19a80e822c46a1f158f97828347b2
courtsmallclass <courtthecourt@gmail.com>
2021-02-08 09:04:49 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dig4639-ex2
+
+"I know the sky is not the limit because there are footprints on the Moon -- and I made some of them!" -Buzz Aldrin
---
+++ index.js
@@ -0,0 +1,8 @@
+let quotes = [
+	'"I know the sky is not the limit because there are footprints on the Moon -- and I made some of them!" -Buzz Aldrin',
+	'"Remember to look up at the stars and not down at your feet. Try to make sense of what you see and wonder about what makes the universe exist. Be curious. And however difficult life may seem, there is always something you can do and succeed at. It matters that you do not just give up." -Stephen Hawking',
+	'"Across the sea of space, the stars are other suns." -Carl Sagan',
+];
+
+let num = Math.floor(Math.random() * 3);
+console.log(quotes[num]);
